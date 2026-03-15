69ee0967008b7c89a4dbd65fa71a7dadab378f45
Gehad-M0hamed <ge73d2562005@gmail.com>
2026-03-15 14:08:57 +0200

    Question 3

--- G-NET-26-C#3/Program.cs
+++ G-NET-26-C#3/Program.cs
@@ -24,66 +24,106 @@ namespace G_NET_26_C_3
             #endregion
 
             #region Question 2:
-    //        Console.Write("Enter your age: ");
-    //        int age = int.Parse(Console.ReadLine());
+            //        Console.Write("Enter your age: ");
+            //        int age = int.Parse(Console.ReadLine());
 
-    //        Console.Write("Enter day of week (1-7, where 7=Fri): ");
-    //        int day = int.Parse(Console.ReadLine());
+            //        Console.Write("Enter day of week (1-7, where 7=Fri): ");
+            //        int day = int.Parse(Console.ReadLine());
 
-    //        Console.Write("Do you have a valid student ID? (yes/no): ");
-    //        string studentIdInput = Console.ReadLine().ToLower();
-    //        bool isStudent = (studentIdInput == "yes");
+            //        Console.Write("Do you have a valid student ID? (yes/no): ");
+            //        string studentIdInput = Console.ReadLine().ToLower();
+            //        bool isStudent = (studentIdInput == "yes");
 
-    //        double price = 0;
-    //        string breakdown = "";
+            //        double price = 0;
+            //        string breakdown = "";
 
-    //        if (age < 5)
-    //        {
-    //            price = 0;
-    //            breakdown = "Age < 5: Free";
-    //        }
-    //        else if (age >= 5 && age <= 12)
-    //        {
-    //            price = 30;
-    //            breakdown = "Age 5-12: 30 LE";
-    //        }
-    //        else if (age >= 13 && age <= 59)
-    //        {
-    //            price = 50;
-    //            breakdown = "Age 13-59: 50 LE";
-    //        }
-    //        else // age 60+
-    //        {
-    //            price = 25;
-    //            breakdown = "Age 60+: 25 LE";
-    //        }
-    //        if (price > 0 && (day == 6 || day == 7))
-    //        {
-    //            price += 10;
-    //            breakdown += "\nWeekend Surcharge: +10 EGP";
-    //        }
+            //        if (age < 5)
+            //        {
+            //            price = 0;
+            //            breakdown = "Age < 5: Free";
+            //        }
+            //        else if (age >= 5 && age <= 12)
+            //        {
+            //            price = 30;
+            //            breakdown = "Age 5-12: 30 LE";
+            //        }
+            //        else if (age >= 13 && age <= 59)
+            //        {
+            //            price = 50;
+            //            breakdown = "Age 13-59: 50 LE";
+            //        }
+            //        else // age 60+
+            //        {
+            //            price = 25;
+            //            breakdown = "Age 60+: 25 LE";
+            //        }
+            //        if (price > 0 && (day == 6 || day == 7))
+            //        {
+            //            price += 10;
+            //            breakdown += "\nWeekend Surcharge: +10 EGP";
+            //        }
 
-    //        if (price > 0 && isStudent)
-    //        {
-    //            double discount = price * 0.20;
-    //            price -= discount;
-    //            breakdown += $"\nStudent Discount (20%): -{discount} LE";
-    //        }
+            //        if (price > 0 && isStudent)
+            //        {
+            //            double discount = price * 0.20;
+            //            price -= discount;
+            //            breakdown += $"\nStudent Discount (20%): -{discount} LE";
+            //        }
 
-    //        Console.WriteLine("\n--- Price Breakdown ---");
-    //        Console.WriteLine(breakdown);
-    //        Console.WriteLine("-----------------------");
-    //        Console.WriteLine($"Final Ticket Price: {price} LE");
-    //    }
-    //}
+            //        Console.WriteLine("\n--- Price Breakdown ---");
+            //        Console.WriteLine(breakdown);
+            //        Console.WriteLine("-----------------------");
+            //        Console.WriteLine($"Final Ticket Price: {price} LE");
+            //    }
+            //}
 
 
             #endregion
 
-            
+            #region Question 3:
+            //(a)
+            //string fileExtension = ".pdf";
+            //string fileType;
+            //switch (fileExtension)
+            //{
+            //    case ".pdf":
+            //        fileType = "PDF Document";
+            //        break;
+            //    case ".docx":
+            //    case ".doc":
+            //        fileType = "Word Document";
+            //        break;
+            //    case ".xlsx":
+            //    case ".xls":
+            //        fileType = "Excel Spreadsheet";
+            //        break;
+            //    case ".jpg":
+            //    case ".png":
+            //    case ".gif":
+            //        fileType = "Image File";
+            //        break;
+            //    default:
+            //        fileType = "Unknown File Type";
+            //        break;
+            //}
+            ////(b)
+            //string fileExtension = ".pdf";
+
+            //string fileType = fileExtension switch
+            //{
+            //    ".pdf" => "PDF Document",
+            //    ".docx" or ".doc" => "Word Document",
+            //    ".xlsx" or ".xls" => "Excel Spreadsheet",
+            //    ".jpg" or ".png" or ".gif" => "Image File",
+            //    _ => "Unknown File Type"
+            //};
+
+            #endregion
 
 
 
-}
+
+
+        }
     }
 }
