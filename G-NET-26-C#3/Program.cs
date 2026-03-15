407cb3edf7e9b2fd44f74d4a263e932a419c919a
Gehad-M0hamed <ge73d2562005@gmail.com>
2026-03-15 03:49:33 +0200

    Question 1:

--- G-NET-26-C#3/Program.cs
+++ G-NET-26-C#3/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace G_NET_26_C_3
+﻿using Microsoft.VisualBasic;
+using System.Diagnostics.Metrics;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace G_NET_26_C_3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 1:
+
+            //(a)immutable, meaning its contents cannot be changed after creation.
+            //old string object remains in memory until the Garbage Collector(GC) removes it.
+            //(b)
+            // StringBuilder sb = new StringBuilder(); 
+            //for (int i = 1; i <= 5000; i++)
+            //{
+            //    sb.Append("PROD-").Append(i).Append(","); 
+            //}
+            //string productList = sb.ToString(); 
+            ////(c)
+            #endregion
+
+
         }
     }
 }
